2864fae88986cec0f4ed7e9745d51acbab33e799
gouthm.04 <gouthm.04@gmail.com>
2026-07-15 00:57:57 +0530

    add view

--- app.js
+++ app.js
@@ -40,6 +40,15 @@ app.get("/test", (req, res) => {
 }
 )
 
+app.post("/add-pet", async (req, res) => {
+    try {
+        await Pet.create(req.body)
+        res.json({ "status": "Success" })
+    } catch (error) {
+        res.status(500).json({ "status": "Error", "message": error.message })
+    }
+})
+
 app.listen(3000, () => {
     console.log("Server Started")
 })
